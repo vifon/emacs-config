4aa84e1cae2495b5debbb390f603dfa378e42b1f
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-02-19 00:17:58 +0100

    registers cleaned up

--- emacs.d/vifon/my-registers.el
+++ emacs.d/vifon/my-registers.el
@@ -1,5 +1,4 @@
 (set-register ?p `(file . ,(concat org-directory "/projects.org")))
 (set-register ?k `(file . ,org-default-notes-file))
-(set-register ?j `(file . ,(concat org-directory "/journal.org")))
 
 (provide 'my-registers)
