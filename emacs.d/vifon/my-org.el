f2966a5686baafe8fd94607244856940057e9de7
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-03-01 11:21:11 +0100

    org-mode: C-c C-1 inserts the timestamp

--- emacs.d/vifon/my-org.el
+++ emacs.d/vifon/my-org.el
@@ -1,7 +1,11 @@
 (use-package org
   :config (progn
             (define-key org-mode-map (kbd "<C-tab>") nil)
-            (define-key org-mode-map (kbd "C-c C-x h") 'helm-org-in-buffer-headings)))
+            (define-key org-mode-map (kbd "C-c C-x h") 'helm-org-in-buffer-headings)
+            (define-key org-mode-map (kbd "C-c C-1")
+              #'(lambda ()
+                  (interactive)
+                  (org-time-stamp-inactive '(16))))))
 
 (use-package org-attach
   :commands (org-attach-expand-link org-attach-attach)
