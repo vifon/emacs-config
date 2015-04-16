a0c5f4af5e801ba8cba33b155170e9a440ef69cb
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-04-16 23:56:42 +0200

    org-mode hydra

--- emacs.d/vifon/my-org.el
+++ emacs.d/vifon/my-org.el
@@ -131,6 +131,61 @@
 
 (plist-put org-format-latex-options :scale 2.0)
 
+(defhydra hydra-org-template (:color blue :hint nil)
+  "
+_c_enter  _q_uote     _e_macs-lisp    _L_aTeX:
+_l_atex   _E_xample   _p_erl          _i_ndex:
+_a_scii   _v_erse     _P_erl tangled  _I_NCLUDE:
+_s_rc     ^ ^         plant_u_ml      _H_TML:
+_h_tml    ^ ^         _S_hell         _A_SCII:
+"
+  ("s" (hot-expand "<s"))
+  ("E" (hot-expand "<e"))
+  ("q" (hot-expand "<q"))
+  ("v" (hot-expand "<v"))
+  ("c" (hot-expand "<c"))
+  ("l" (hot-expand "<l"))
+  ("h" (hot-expand "<h"))
+  ("a" (hot-expand "<a"))
+  ("L" (hot-expand "<L"))
+  ("i" (hot-expand "<i"))
+  ("S" (progn
+         (hot-expand "<s")
+         (insert "sh :results raw drawer")
+         (forward-line)))
+  ("e" (progn
+         (hot-expand "<s")
+         (insert "emacs-lisp")
+         (forward-line)))
+  ("p" (progn
+         (hot-expand "<s")
+         (insert "perl")
+         (forward-line)))
+  ("u" (progn
+         (hot-expand "<s")
+         (insert "plantuml :file CHANGE.png")
+         (forward-line)))
+  ("P" (progn
+         (insert "#+HEADERS: :results output :exports both :shebang \"#!/usr/bin/env perl\"\n")
+         (hot-expand "<s")
+         (insert "perl")
+         (forward-line)))
+  ("I" (hot-expand "<I"))
+  ("H" (hot-expand "<H"))
+  ("A" (hot-expand "<A"))
+  ("<" self-insert-command "ins")
+  ("o" nil "quit"))
+
+(defun hot-expand (str)
+  "Expand org template."
+  (insert str)
+  (org-try-structure-completion))
+
+(define-key org-mode-map "<"
+  (lambda () (interactive)
+    (if (looking-back "^")
+        (hydra-org-template/body)
+        (self-insert-command 1))))
 
 
 (use-package ox
