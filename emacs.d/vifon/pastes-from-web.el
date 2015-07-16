43d4da5e0e6dfeed124f7c2fd87367b7311dccf1
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-07-16 23:55:03 +0200

    disabled the fullscreen Magit advices

--- emacs.d/vifon/pastes-from-web.el
+++ emacs.d/vifon/pastes-from-web.el
@@ -24,19 +24,19 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
+;; (defadvice magit-status (around magit-fullscreen activate)
+;;   (window-configuration-to-register :magit-fullscreen)
+;;   ad-do-it
+;;   (delete-other-windows))
 
-(defun magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
+;; (defun magit-quit-session ()
+;;   "Restores the previous window configuration and kills the magit buffer"
+;;   (interactive)
+;;   (kill-buffer)
+;;   (jump-to-register :magit-fullscreen))
 
-(eval-after-load "magit"
-  '(define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
+;; (eval-after-load "magit"
+;;   '(define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
