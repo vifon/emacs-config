a1cf09ca1f46bed2f9ce395e73ac3ad38086ac93
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2016-03-30 16:33:04 +0200

    Add isearch-exit-other-end

--- emacs.d/vifon/my-keys.el
+++ emacs.d/vifon/my-keys.el
@@ -216,4 +216,14 @@ _i_spell-change-dictionary: %(when (boundp 'ispell-current-dictionary) ispell-cu
                   ("SPC" nil "quit")))
 
 
+;;; From https://www.emacswiki.org/emacs/ZapToISearch
+(defun isearch-exit-other-end (rbeg rend)
+  "Exit isearch, but at the other end of the search string.
+  This is useful when followed by an immediate kill."
+  (interactive "r")
+  (isearch-exit)
+  (goto-char isearch-other-end))
+(define-key isearch-mode-map [(control return)] 'isearch-exit-other-end)
+
+
 (provide 'my-keys)
