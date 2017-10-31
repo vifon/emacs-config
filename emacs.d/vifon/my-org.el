7c74dd04a0880a688a5c53c2430fac4cc9fcd882
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2017-10-31 21:27:58 +0100

    Changes needed for the new org-babel version

--- emacs.d/vifon/my-org.el
+++ emacs.d/vifon/my-org.el
@@ -252,9 +252,8 @@ _h_tml    ^ ^         _S_hell         _A_SCII:
             (setq org-confirm-babel-evaluate nil)
             (org-babel-do-load-languages
              'org-babel-load-languages
-             '((shell . t)
+             '((sh . t)
                (awk . t)
-               (sed . t)
                (makefile . t)
 
                (ditaa . t)
