74bb58ca1091148678a8b86ffe961de8ac3df4f5
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2018-03-08 14:21:59 +0100

    Create one package directory per Emacs version

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,6 +9,9 @@
   (if (locate-library "package")
       (progn
         (require 'package)
+        (setq package-user-dir (concat
+                                (file-name-as-directory package-user-dir)
+                                emacs-version))
         (setq package-archives
               '(("gnu"       . "https://elpa.gnu.org/packages/")
                 ("marmalade" . "https://marmalade-repo.org/packages/")
