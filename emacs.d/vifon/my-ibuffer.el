bb341b483dcc1c284be13d60155ba80943069a9f
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2018-04-19 23:35:17 +0200

    Clean up and remove the unused parts of the ibuffer config

--- emacs.d/vifon/my-ibuffer.el
+++ emacs.d/vifon/my-ibuffer.el
@@ -1,43 +1,26 @@
-(setq ibuffer-saved-filter-groups '(("vifon"
-                                     ("terminals" (or (mode . eshell-mode)
-                                                      (mode . term-mode)))
-                                     ("*emacs*" (or (name . "^\\*.*\\*$")
-                                                    (name . "^\\*.*\\*<[0-9]+>$")))
-                                     ("emacs-config" (or (filename . "\\.emacs\\.d")
-                                                         (filename . "emacs-config")
-                                                         (filename . "\\.emacs")
-                                                         (mode . emacs-lisp-mode)))
-                                     ("Documents" (or (mode . doc-view-mode)
-                                                      (mode . image-mode)))
-                                     ("dired" (or (mode . dired-mode)
-                                                  (mode . wdired-mode)))
-                                     ("org" (mode . org-mode)))))
-
-(add-hook 'ibuffer-mode-hook
-          (lambda ()
-            (setq ibuffer-sorting-mode 'filename/process)))
-
-(setq ibuffer-show-empty-filter-groups nil)
-(setq ibuffer-expert t)
-(setq ibuffer-formats '((mark modified read-only " "
-                              (name 32 32 :left :elide)
-                              " "
-                              (size 9 -1 :right)
-                              " "
-                              (mode 16 64 :left :elide)
-                              " " filename-and-process)
-                        (mark " "
-                              (name 32 -1)
-                              " " filename)))
-
-(global-set-key (kbd "C-x C-b") #'ibuffer)
+(use-package ibuffer
+  :bind (("C-x C-b" . ibuffer)
+         :map ibuffer-mode-map
+         ("/ V" . ibuffer-vc-set-filter-groups-by-vc-root))
+  :config (progn
+            (add-hook 'ibuffer-mode-hook
+                      (lambda ()
+                        (setq ibuffer-sorting-mode 'filename/process)))
+            (setq ibuffer-show-empty-filter-groups nil)
+            (setq ibuffer-expert t)
+            (setq ibuffer-formats '((mark modified read-only " "
+                                          (name 32 32 :left :elide)
+                                          " "
+                                          (size 9 -1 :right)
+                                          " "
+                                          (mode 16 64 :left :elide)
+                                          " " filename-and-process)
+                                    (mark " "
+                                          (name 32 -1)
+                                          " " filename)))))
 
 (use-package ibuffer-vc
   :ensure t
   :commands ibuffer-vc-set-filter-groups-by-vc-root)
-
-(use-package ibuffer
-  :config (define-key ibuffer-mode-map
-            (kbd "/ V") #'ibuffer-vc-set-filter-groups-by-vc-root))
 
 (provide 'my-ibuffer)
