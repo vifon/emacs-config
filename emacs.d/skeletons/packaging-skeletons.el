9ffd1f09ebae53ac2333adf0ca7a087ae351ac12
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2018-11-24 15:08:36 +0100

    Add skeletons for software packaging, refactor the skeleton loading

---
+++ emacs.d/skeletons/packaging-skeletons.el
@@ -0,0 +1,42 @@
+(define-skeleton ebuild-skeleton
+  "" ""
+  "# Copyright 1999-2018 Gentoo Foundation\n"
+  "# Distributed under the terms of the GNU General Public License v2\n"
+  "\n"
+  "EAPI=7\n"
+  "\n"
+  "DESCRIPTION=\"\"\n"
+  "HOMEPAGE=\"\"\n"
+  "SRC_URI=\"\"\n"
+  "\n"
+  "LICENSE=\"\"\n"
+  "SLOT=\"0\"\n"
+  "KEYWORDS=\"~amd64 ~x86\"\n"
+  "IUSE=\"\"\n"
+  "\n"
+  "DEPEND=\"\"\n"
+  "RDEPEND=\"${DEPEND}\"\n"
+  "BDEPEND=\"\"\n"
+  _
+  )
+(define-auto-insert "\\.ebuild$" 'ebuild-skeleton)
+
+(define-skeleton cruxpkg-skeleton
+  "" ""
+  "name=" (file-name-nondirectory (directory-file-name default-directory)) "\n"
+  "version=" (skeleton-read "Version: ") "\n"
+  "release=1\n"
+  "source=(" _ ")\n"
+  "\n"
+  "build() {\n"
+  "    cd $name-$version\n"
+  (if (y-or-n-p "Does the project use ./configure?")
+      (concat "    ./configure --prefix=$HOME/local --disable-nls\n"
+              "    make\n"
+              "    make DESTDIR=$PKG install\n")
+    (concat "    make PREFIX=$HOME/local\n"
+            "    make PREFIX=$HOME/local DESTDIR=$PKG install\n"))
+  "    rm -rf $PKG/usr/info\n"
+  "}\n"
+  )
+(define-auto-insert "/Pkgfile$" 'cruxpkg-skeleton)
--- emacs.d/vifon/my-skeletons.el
+++ emacs.d/vifon/my-skeletons.el
@@ -1,21 +1,5 @@
-(add-to-list 'load-path "~/.emacs.d/skeletons")
-(load "~/.emacs.d/skeletons/asm-skeleton.el")
-(load "~/.emacs.d/skeletons/c++-skeleton.el")
-(load "~/.emacs.d/skeletons/cmake-skeleton.el")
-(load "~/.emacs.d/skeletons/docker-skeleton.el")
-(load "~/.emacs.d/skeletons/generic.el")
-(load "~/.emacs.d/skeletons/haskell-skeleton.el")
-(load "~/.emacs.d/skeletons/java-skeleton.el")
-(load "~/.emacs.d/skeletons/makefile-skeleton.el")
-(load "~/.emacs.d/skeletons/markdown-skeleton.el")
-(load "~/.emacs.d/skeletons/org-skeleton.el")
-(load "~/.emacs.d/skeletons/other-skeletons.el")
-(load "~/.emacs.d/skeletons/perl-skeleton.el")
-(load "~/.emacs.d/skeletons/python-skeleton.el")
-(load "~/.emacs.d/skeletons/rust-skeleton.el")
-(load "~/.emacs.d/skeletons/shell-skeleton.el")
-(load "~/.emacs.d/skeletons/tex-skeleton.el")
-(load "~/.emacs.d/skeletons/web-skeleton.el")
+(dolist (skeleton-file (directory-files "~/.emacs.d/skeletons/" t ".elc?$"))
+  (load skeleton-file))
 
 (use-package skeletor
   :ensure t
