8497b41353081f7a60aba21c59ace1ad664b15f1
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2019-04-25 00:55:30 +0200

    New snippet: CSS Grid

---
+++ emacs.d/snippets/web-css-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `web-css-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'web-css-mode
+                     '(("grid" "display: grid;\ngrid-template-columns: ${1:1fr};\ngrid-template-rows: ${2:1fr};\ngrid-template-areas:\n\"$0\"\n;" "grid" nil nil nil "/home/vifon/.emacs.d/snippets/web-css-mode/grid" nil nil)))
+
+
+;;; Do not edit! File generated at Tue Apr  9 22:37:36 2019
