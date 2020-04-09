8416ea2b3dcc2109e5a646a2afaaabd238fb54f8
Wojciech Siewierski <wojciech@siewierski.eu>
2020-04-09 03:01:47 +0200

    Stop keeping the yasnippet compiled snippets in the repo

--- emacs.d/snippets/web-css-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `web-css-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'web-css-mode
-                     '(("grid" "display: grid;\ngrid-template-columns: ${1:1fr};\ngrid-template-rows: ${2:1fr};\ngrid-template-areas:\n\"$0\"\n;" "grid" nil nil nil "/home/vifon/.emacs.d/snippets/web-css-mode/grid" nil nil)))
-
-
-;;; Do not edit! File generated at Thu Jan 23 18:55:15 2020
