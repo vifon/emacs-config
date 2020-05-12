fb6ad586d0b0001d5e5a6e1404204acd901648ae
Wojciech Siewierski <wojciech@siewierski.eu>
2020-05-12 17:25:32 +0200

    Reload the font on theme change
    Otherwise *sometimes* the font becomes very tiny.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -978,6 +978,15 @@ ignore) any passed arguments to work as an advice."
 (add-hook 'minibuffer-exit-hook #'my-minibuffer-exit-hook)
 
 
+(defun vifon/set-font ()
+  (interactive)
+  (ignore-errors
+    (let* ((font-name "JetBrains Mono")
+           (font-size "13")
+           (font (concat font-name "-" font-size)))
+      (add-to-list 'default-frame-alist `(font . ,font))
+      (set-frame-font font nil t))))
+
 (use-package solarized-theme
   :ensure t
   :config (let ((vifon/theme-light 'solarized-light)
@@ -987,28 +996,24 @@ ignore) any passed arguments to work as an advice."
               (interactive)
               (disable-theme vifon/theme-dark)
               (setq frame-background-mode 'light)
-              (load-theme vifon/theme-light 'no-confirm))
+              (load-theme vifon/theme-light 'no-confirm)
+              (vifon/set-font))
             (defun vifon/theme-dark ()
               (interactive)
               (disable-theme vifon/theme-light)
               (setq frame-background-mode 'dark)
-              (load-theme vifon/theme-dark 'no-confirm))
+              (load-theme vifon/theme-dark 'no-confirm)
+              (vifon/set-font))
 
             (setq solarized-scale-org-headlines nil
                   solarized-scale-outline-headlines nil
                   solarized-use-variable-pitch nil)
             (setq frame-background-mode 'light)
             (load-theme vifon/theme-light 'no-confirm)
+            (vifon/set-font)
 
             (global-set-key (kbd "C-M-s-<") #'vifon/theme-light)
             (global-set-key (kbd "C-M-s->") #'vifon/theme-dark)))
-
-(ignore-errors
-  (let* ((font-name "JetBrains Mono")
-         (font-size "13")
-         (font (concat font-name "-" font-size)))
-    (add-to-list 'default-frame-alist `(font . ,font))
-    (set-frame-font font nil t)))
 
 
 ;;; DEPRECATION: replaced by "~/.emacs.d/local.el"
