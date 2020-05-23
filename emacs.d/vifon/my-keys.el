fe12edfc75ce6efd0677e92eabfc5ce300273db7
Wojciech Siewierski <wojciech@siewierski.eu>
2020-05-23 19:22:54 +0200

    Remove an obsolete command
    I can achieve the same by pressing C-r before RET.

--- emacs.d/vifon/my-keys.el
+++ emacs.d/vifon/my-keys.el
@@ -235,14 +235,6 @@ _0__a_↤  _h__i__l_  ↦_e_
                   ("SPC" nil)))
 
 
-;;; From https://www.emacswiki.org/emacs/ZapToISearch
-(defun isearch-exit-other-end (rbeg rend)
-  "Exit isearch, but at the other end of the search string.
-  This is useful when followed by an immediate kill."
-  (interactive "r")
-  (isearch-exit)
-  (goto-char isearch-other-end))
-(define-key isearch-mode-map [(control return)] #'isearch-exit-other-end)
 
 
 (provide 'my-keys)
