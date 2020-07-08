0821476025eba5f82ed173ca0c14a7123ee0e355
Wojciech Siewierski <wojciech@siewierski.eu>
2020-07-08 03:42:51 +0200

    A personalized scratch-mode for the scratch buffer

---
+++ emacs.d/modules/scratch-mode.el
@@ -0,0 +1,73 @@
+(defgroup scratch-mode nil
+  "A specialized mode for the scratch buffer.")
+
+(defun scratch-reset ()
+  (interactive)
+  (switch-to-buffer "*scratch*")
+  (scratch-mode))
+
+(defvar scratch-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "o") #'org-mode)
+    (define-key map (kbd "e") #'lisp-interaction-mode)
+    (define-key map (kbd "m") #'markdown-mode)
+    (define-key map (kbd "i") (lambda ()
+                                (interactive)
+                                (find-file "~/.emacs.d/init.el")))
+    (define-key map (kbd "C") (lambda ()
+                                (interactive)
+                                (find-library "scratch-mode")))
+    (define-key map (kbd "p") #'projectile-switch-project)
+    (define-key map (kbd "SPC") #'notmuch)
+    (define-key map (kbd "z") #'deft)
+    (define-key map (kbd "C-c C-x C-j") #'org-clock-goto)
+    (define-key map (kbd "j") #'org-clock-goto)
+    (define-key map (kbd "J") #'org-mru-clock-select-recent-task)
+    (define-key map (kbd "C-'") #'org-cycle-agenda-files)
+    (define-key map (kbd "a") #'org-agenda)
+    (define-key map (kbd "g") #'scratch-reset)
+    (define-key map (kbd "s") #'scratch-dir)
+    map))
+
+(defcustom scratch-mode-key-hints
+  '("o"
+    "e"
+    "m"
+    "i"
+    "p"
+    "SPC"
+    "z"
+    "a"
+    "j"
+    "J"
+    "s")
+  "The keymap hints to show in `scratch-mode'."
+  :type '(list key-sequence))
+
+(define-derived-mode scratch-mode special-mode "scratch"
+  "A dedicated scratch buffer mode with commonly used commands bound."
+  (setq font-lock-defaults
+        '((("^\\(.*?\\):" 1 'bold)
+           (": \\(.*\\)$" 1 'italic))))
+
+  (setq cursor-type nil)
+
+  (emacs-lock-mode 'kill)
+  (cd "~/")
+
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (dolist (key scratch-mode-key-hints)
+      (let ((command (local-key-binding (kbd key))))
+        (insert (format "%s: %s\n"
+                        key
+                        command)))))
+  (set-buffer-modified-p nil)
+  (goto-char (point-min))
+  (add-hook 'change-major-mode-hook
+            (lambda ()
+              (read-only-mode 0)
+              (erase-buffer))
+            nil t))
+
+(provide 'scratch-mode)
--- emacs.d/vifon/my-scratch.el
+++ emacs.d/vifon/my-scratch.el
@@ -1,39 +1,6 @@
-(when (featurep 'org)
-  (setq initial-major-mode 'org-mode))
-
-(defun scratch-reset-message ()
-  (setq initial-scratch-message
-        (if (file-exists-p "~/.emacs.d/scratch.org")
-            (with-temp-buffer
-              (insert-file-contents "~/.emacs.d/scratch.org")
-              (buffer-string))
-          "")))
-
-(defun scratch-reset (&optional arg)
-  (interactive "P")
-  (switch-to-buffer "*scratch*")
-  (emacs-lock-mode 'kill)
-  (cd "~/")
-  (cond
-   ((equal arg '(16))
-    ;; Emacs Lisp scratchpad.
-    (erase-buffer)
-    (insert "()")
-    (lisp-interaction-mode)
-    (set-buffer-modified-p nil)
-    (goto-char (1+ (point-min))))
-   ((or (equal arg '(4))
-        (= (point-min) (point-max)))
-    ;; General purpose org-mode scratchpad.
-    (erase-buffer)
-    (insert (scratch-reset-message))
-    (funcall initial-major-mode)
-    (set-buffer-modified-p nil)
-    (goto-char (point-min)))))
+(require 'scratch-mode)
 
 (global-set-key (kbd "C-c s") #'scratch-reset)
-
-(add-hook 'after-init-hook
-          #'scratch-reset)
+(setq initial-major-mode 'scratch-mode)
 
 (provide 'my-scratch)
