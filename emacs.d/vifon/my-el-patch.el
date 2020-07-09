6cf51ce723de7481ada93c216337d06f211ddf4f
Wojciech Siewierski <wojciech@siewierski.eu>
2020-07-09 03:02:26 +0200

    Update el-patch definitions
    Honestly, I don't recall further details, so sorry, future me, for
    this vague commit message.

--- emacs.d/vifon/my-el-patch.el
+++ emacs.d/vifon/my-el-patch.el
@@ -2,54 +2,72 @@
   :ensure t
   :config (setq el-patch-use-aggressive-defvar t))
 
-(el-patch-feature ledger-complete)
-(with-eval-after-load 'ledger-complete
-  (el-patch-defun ledger-fully-complete-xact ()
-    "Completes a transaction if there is another matching payee in the buffer.
-Does not use ledger xact"
-    (interactive)
-    (let* ((name (ledger-trim-trailing-whitespace (caar (ledger-parse-arguments))))
-           (rest-of-name name)
-           xacts)
-      (save-excursion
-        (when (eq 'transaction (ledger-thing-at-point))
-          (delete-region (point) (+ (length name) (point)))
-          ;; Search backward for a matching payee
-          (el-patch-add (require 'cl-extra))
-          (when (el-patch-wrap 1 1
-                  (cl-some (el-patch-wrap 2
-                             (lambda (x)
-                               (re-search-backward
-                                (concat (el-patch-concat
-                                          "^[0-9/.=-]+\\(\\s-+\\*\\)?\\(\\s-+(.*?)\\)?\\s-+\\("
-                                          (el-patch-remove ".*"))
-                                        (el-patch-add (car x))
-                                        (regexp-quote name)
-                                        (el-patch-swap ".*\\)"
-                                                       (cdr x))
-                                        (el-patch-add "\\)"))
-                                nil t)))
-                           '(("" . "$") (".*" . ".*"))))
-            (setq rest-of-name (match-string 3))
-            ;; Start copying the postings
-            (forward-line)
-            (while (looking-at ledger-account-any-status-regex)
-              (setq xacts (cons (buffer-substring-no-properties
-                                 (line-beginning-position)
-                                 (line-end-position))
-                                xacts))
-              (forward-line))
-            (setq xacts (nreverse xacts)))))
-      ;; Insert rest-of-name and the postings
-      (when xacts
-        (save-excursion
-          (insert rest-of-name ?\n)
-          (while xacts
-            (insert (car xacts) ?\n)
-            (setq xacts (cdr xacts))))
-        (forward-line)
-        (goto-char (line-end-position))
-        (if (re-search-backward "\\(\t\\| [ \t]\\)" nil t)
-            (goto-char (match-end 0)))))))
+(el-patch-feature dired)
+(with-eval-after-load 'dired
+  (el-patch-defun dired-copy-filename-as-kill (&optional arg)
+    "Copy names of marked (or next ARG) files into the kill ring.
+The names are separated by a space.
+With a zero prefix arg, use the absolute file name of each marked file.
+With \\[universal-argument], use the file name relative to the Dired buffer's
+`default-directory'.  (This still may contain slashes if in a subdirectory.)
+
+If on a subdir headerline, use absolute subdirname instead;
+prefix arg and marked files are ignored in this case.
+
+You can then feed the file name(s) to other commands with \\[yank]."
+    (interactive "P")
+    (let ((string
+           (or (dired-get-subdir)
+               (mapconcat (el-patch-wrap 3
+                            (if (equal arg '(16))
+                                #'shell-quote-argument
+                              #'identity))
+                          (if arg
+                              (cond ((zerop (prefix-numeric-value arg))
+                                     (dired-get-marked-files))
+                                    ((consp arg)
+                                     (dired-get-marked-files t))
+                                    (t
+                                     (dired-get-marked-files
+                                      'no-dir (prefix-numeric-value arg))))
+                            (dired-get-marked-files 'no-dir))
+                          " "))))
+      (unless (string= string "")
+        (if (eq last-command 'kill-region)
+            (kill-append string nil)
+          (kill-new string))
+        (message "%s" string)))))
+
+(el-patch-feature dired-subtree)
+(with-eval-after-load 'dired-subtree
+  (el-patch-defun dired-subtree--readin (dir-name)
+    "Read in the directory.
+
+Return a string suitable for insertion in `dired' buffer."
+    (el-patch-wrap 2
+      (let ((orig-buffer (current-buffer)))
+        (with-temp-buffer
+          (insert-directory dir-name
+                            (el-patch-swap
+                              dired-listing-switches
+                              (buffer-local-value 'dired-actual-switches
+                                                  orig-buffer))
+                            nil t)
+          (delete-char -1)
+          (goto-char (point-min))
+          (delete-region
+           (progn (beginning-of-line) (point))
+           (progn (forward-line
+                   (if (save-excursion
+                         (forward-line 1)
+                         (end-of-line)
+                         (looking-back "\\."))
+                       3 1))
+                  (point)))
+          (insert "  ")
+          (while (= (forward-line) 0)
+            (insert "  "))
+          (delete-char -2)
+          (buffer-string))))))
 
 (provide 'my-el-patch)
