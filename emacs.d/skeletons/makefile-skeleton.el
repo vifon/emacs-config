b32c3b59fc43d252558de7c31a5d464f8fc77966
Wojciech Siewierski <wojciech@siewierski.eu>
2020-07-16 03:13:13 +0200

    Unearth my ancient Makefile skeleton and make it less terrible

--- emacs.d/skeletons/makefile-skeleton.el
+++ emacs.d/skeletons/makefile-skeleton.el
@@ -1,34 +1,10 @@
 (define-skeleton makefile-skeleton
   "" ""
-  (let ((sources (mapconcat 'identity (merge 'list (file-expand-wildcards "*.c") (file-expand-wildcards "*.cpp") 'string-lessp) " "))
-        (headers (mapconcat 'identity (merge 'list (file-expand-wildcards "*.h") (file-expand-wildcards "*.hpp") 'string-lessp) " ")))
-    (concat
-     "O=2
-STD=c++11
-CFLAGS=-Wall -Wextra -Weffc++ -O$(O) -std=$(STD)
-LFLAGS=
-
-SOURCES="sources"
-HEADERS="headers"
-OUT=a.out
-
-.PHONY: all
-all: release
-
-.PHONY: release
-release: $(OUT)
-
-.PHONY: debug
-debug: O=0
-debug: CFLAGS+=-g
-debug: $(OUT)
-
-$(OUT): $(SOURCES) $(HEADERS)
-	$(CXX) $(CFLAGS) $(SOURCES) $(LFLAGS) -o $(OUT)
-
-.PHONY: clean
-clean:
-	rm -fv $(OUT) *.o
-")))
+  "CCFLAGS=-Wall -Wextra\n"
+  "LDFLAGS=\n"
+  "LDLIBS=\n"
+  "\n"
+  ".PHONY: all\n"
+  "all: " (skeleton-read "Output file: " "main") "\n")
 
 (define-auto-insert "/Makefile$" 'makefile-skeleton)
