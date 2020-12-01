64ec34af8586d00c191297fe01536819757f2d85
Wojciech Siewierski <wojciech@siewierski.eu>
2020-12-01 04:17:19 +0100

    Overhaul the Nix skeletons

--- emacs.d/skeletons/nix-skeleton.el
+++ emacs.d/skeletons/nix-skeleton.el
@@ -1,34 +1,66 @@
 (define-skeleton nix-shell-skeleton
   "" ""
   "#!/usr/bin/env nix-shell\n"
-  "\n"
   "{ pkgs ? import <nixpkgs> {} }:\n"
+  "\n"
   "with pkgs; mkShell {\n"
   "  buildInputs = [\n"
   "    " _ "\n"
   "  ];\n"
-  "  shellHook = ''\n"
-  "  '';\n"
+  "}\n"
+  )
+
+(define-skeleton nix-docker-skeleton
+  "" ""
+  "{ pkgs ? import <nixpkgs> {} }:\n"
+  "\n"
+  "with pkgs;\n"
+  "dockerTools.buildImage {\n"
+  "  name = \"" (skeleton-read "Image name: "
+                                (file-name-nondirectory
+                                 (directory-file-name
+                                  default-directory))) "\";\n"
+  "  tag = \"latest\";\n"
+  "\n"
+  "  contents = [ coreutils" _ " ];\n"
+  "\n"
+  "  config = {\n"
+  "  Cmd = [ \"${bash}/bin/bash\" ];\n"
+  "  };\n"
   "}\n"
   )
 
 (define-skeleton nix-package-skeleton
   "" ""
-  ;; "{ stdenv, fetchurl }:\n"
-  "with import <nixpkgs> {};\n"
-  "stdenv.mkDerivation {\n"
-  "  name = \"example" _ "-1.0\";\n"
-  "  # builder = ./builder.sh;\n"
-  "  src = fetchurl {\n"
-  "    url = https://example.com;\n"
-  "    sha256 = \"\";\n"
-  "  };\n"
+  "{ pkgs ? import <nixpkgs> {} }:\n"
+  "\n"
+  "with pkgs;\n"
+  "stdenv.mkDerivation rec {\n"
+  "  pname = \"" (skeleton-read "Package name: "
+                                (file-name-nondirectory
+                                 (directory-file-name
+                                  default-directory))) "\";\n"
+  "  version = \"" (skeleton-read "Package version: " "1.0") "\";\n"
+  (if (y-or-n-p "Fetch from Github?")
+      (concat "  src = fetchFromGitHub {\n"
+              "    owner = \"" (skeleton-read "Repo owner: " (user-login-name)) "\";\n"
+              "    repo = pname;\n"
+              "    rev = \"v${version}\";\n"
+              "    sha256 = \"\";\n"
+              "  };\n")
+    (concat "  src = fetchurl {\n"
+            "    url = " (setq v1 (skeleton-read "Fetch URL: ")) ";\n"
+            "    sha256 = \"" (when (and (not (string-empty-p v1))
+                                         (y-or-n-p "Fetch & calculate the hash?"))
+                                (shell-command-to-string (concat "nix-prefetch-url "
+                                                                 "\"" v1 "\""
+                                                                 " 2> /dev/null"))) "\";\n"
+            "  };\n"))
   "\n"
   "  buildInputs = [\n"
-  "    pkg-config\n"
+  "    pkg-config" _ "\n"
   "  ];\n"
   "}\n"
-  _
   )
 
 (define-auto-insert "/shell.nix\\'" 'nix-shell-skeleton)
