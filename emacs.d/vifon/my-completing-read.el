0ab2ed36bf28dff99d789f73bbd469dffbdbecfe
Wojciech Siewierski <wojciech@siewierski.eu>
2021-01-15 17:33:08 +0100

    Remove the manual Selectrum & Embark configuration
    According to the Selectrum wiki Embark now works out of the box.

--- emacs.d/vifon/my-completing-read.el
+++ emacs.d/vifon/my-completing-read.el
@@ -29,28 +29,7 @@
   :bind (("C-c o" . embark-act)
          :map minibuffer-local-map
          ("M-o"   . embark-act)
-         ("C-M-o" . embark-act-noexit))
-  :config (progn
-            ;; Source: https://github.com/raxod502/selectrum/wiki/Additional-Configuration#minibuffer-actions-with-embark
-            (progn
-              (require 'selectrum)
-              (defun current-candidate+category ()
-                (when selectrum-active-p
-                  (cons (selectrum--get-meta 'category)
-                        (selectrum-get-current-candidate))))
-
-              (add-hook 'embark-target-finders #'current-candidate+category)
-
-              (defun current-candidates+category ()
-                (when selectrum-active-p
-                  (cons (selectrum--get-meta 'category)
-                        (selectrum-get-current-candidates
-                         ;; Pass relative file names for dired.
-                         minibuffer-completing-file-name))))
-
-              (add-hook 'embark-candidate-collectors #'current-candidates+category)
-              ;; No unnecessary computation delay after injection.
-              (add-hook 'embark-setup-hook 'selectrum-set-selected-candidate))))
+         ("C-M-o" . embark-act-noexit)))
 
 (use-package embark-consult
   :ensure t
