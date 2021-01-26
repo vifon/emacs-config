e93653b31e36108e5853aff98a541738857f7fd9
Wojciech Siewierski <wojciech@siewierski.eu>
2021-01-26 21:14:24 +0100

    Remove an unnecessary debug mode
    debug-on-next-call already makes sure we'll end up in the debug mode.

--- utils/debug-lazy-loading.sh
+++ utils/debug-lazy-loading.sh
@@ -4,7 +4,7 @@ set -o errexit -o nounset -o pipefail
 
 PACKAGE="$1"
 
-emacs --debug-init -Q \
+emacs -Q \
       --eval '(with-eval-after-load "'"$PACKAGE"'" (setq debug-on-next-call t))' \
       -l ~/.emacs.d/early-init.el \
       -l ~/.emacs.d/init.el
