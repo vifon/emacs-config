80656ba59a74b067fcf41646d6f6c6c20cfa1414
Wojciech Siewierski <wojciech@siewierski.eu>
2021-02-01 18:25:20 +0100

    Bash skeleton: Provide an easy to use variable with the script dir
    If it's not needed, it can be easily deleted.  But when it's needed,
    reinventing it with all its quirks is tedious.

--- emacs.d/skeletons/shell-skeleton.el
+++ emacs.d/skeletons/shell-skeleton.el
@@ -2,6 +2,8 @@
   "" ""
   "#!/usr/bin/env bash\n"
   "\n"
+  "SCRIPTDIR=\"$(cd \"$(dirname \"${BASH_SOURCE[0]}\")\" > /dev/null 2>&1 && pwd)\"\n"
+  "\n"
   "set -o errexit -o nounset -o pipefail\n"
   "\n"
   _
