fc9f2f0d7385323843a4f7aec9181fa2345cab99
Wojciech Siewierski <wojciech@siewierski.eu>
2021-02-01 18:23:07 +0100

    Add theme switching keys to scratch-mode

--- emacs.d/vifon/my-scratch.el
+++ emacs.d/vifon/my-scratch.el
@@ -1,5 +1,9 @@
 (require 'scratch-mode)
 
+(define-key scratch-mode-map (kbd ",") #'vifon/theme-light)
+(define-key scratch-mode-map (kbd ".") #'vifon/theme-dark)
+(define-key scratch-mode-map (kbd "/") #'vifon/theme-dwim)
+
 (global-set-key (kbd "C-c s") #'scratch-reset)
 (setq initial-major-mode 'scratch-mode)
 
