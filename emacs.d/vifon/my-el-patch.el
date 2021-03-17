9b42cad193fcf93bea61a84aa603f676e332c15e
Wojciech Siewierski <wojciech@siewierski.eu>
2021-03-17 01:18:22 +0100

    Update the org-clock el-patch against the upstream

--- emacs.d/vifon/my-el-patch.el
+++ emacs.d/vifon/my-el-patch.el
@@ -93,13 +93,13 @@ thus, the heading it's under), and START-TIME is when the clock
 was started."
     (cl-assert clock)
     (let* ((ch
-	        (save-window-excursion
-	          (save-excursion
-	            (unless org-clock-resolving-clocks-due-to-idleness
-		          (org-clock-jump-to-current-clock clock))
-	            (unless org-clock-resolve-expert
-		          (with-output-to-temp-buffer "*Org Clock*"
-		            (princ (format-message "Select a Clock Resolution Command:
+            (save-window-excursion
+              (save-excursion
+                (unless org-clock-resolving-clocks-due-to-idleness
+                  (org-clock-jump-to-current-clock clock))
+                (unless org-clock-resolve-expert
+                  (with-output-to-temp-buffer "*Org Clock*"
+                    (princ (format-message "Select a Clock Resolution Command:
 
 i/q      Ignore this question; the same as keeping all the idle time.
 
@@ -107,6 +107,9 @@ k/K      Keep X minutes of the idle time (default is all).  If this
          amount is less than the default, you will be clocked out
          that many minutes after the time that idling began, and then
          clocked back in at the present time.
+
+t/T      Like `k', but will ask you to specify a time (when you got
+         distracted away), instead of a number of minutes.
 
 g/G      Indicate that you \"got back\" X minutes ago.  This is quite
          different from `k': it clocks you out from the beginning of
@@ -122,26 +125,31 @@ j/J      Jump to the current clock, to make manual adjustments.
 
 For all these options, using uppercase makes your final state
 to be CLOCKED OUT."))))
-	            (org-fit-window-to-buffer (get-buffer-window "*Org Clock*"))
-	            (let (char-pressed)
-		          (while (or (null char-pressed)
-			                 (and (not (memq char-pressed
-					                         '(?k ?K ?g ?G ?s ?S ?C
-						                          ?j ?J ?i ?q)))
-				                  (or (ding) t)))
-		            (setq char-pressed
-			              (read-char (concat (funcall prompt-fn clock)
-					                         " [jkKgGSscCiq]? ")
-				                     nil 45)))
-		          (and (not (memq char-pressed '(?i ?q))) char-pressed)))))
-	       (default
-	         (floor (org-time-convert-to-integer (org-time-since last-valid))
-		            60))
-	       (keep
-	        (and (memq ch '(?k ?K))
-	             (read-number "Keep how many minutes? " default)))
-	       (gotback
-	        (and (memq ch '(?g ?G))
+                (org-fit-window-to-buffer (get-buffer-window "*Org Clock*"))
+                (let (char-pressed)
+                  (while (or (null char-pressed)
+                             (and (not (memq char-pressed
+                                             '(?k ?K ?g ?G ?s ?S ?C
+                                                  ?j ?J ?i ?q ?t ?T)))
+                                  (or (ding) t)))
+                    (setq char-pressed
+                          (read-char (concat (funcall prompt-fn clock)
+                                             " [jkKtTgGSscCiq]? ")
+                                     nil 45)))
+                  (and (not (memq char-pressed '(?i ?q))) char-pressed)))))
+           (default
+             (floor (org-time-convert-to-integer (org-time-since last-valid))
+                    60))
+           (keep
+            (or (and (memq ch '(?k ?K))
+                     (read-number "Keep how many minutes? " default))
+                (and (memq ch '(?t ?T))
+                     (floor
+                      (/ (float-time
+                          (org-time-subtract (org-read-date t t) last-valid))
+                         60)))))
+           (gotback
+            (and (memq ch '(?g ?G))
                  (el-patch-swap
                    (read-number "Got back how many minutes ago? " default)
                    (let ((input (read-string "Got back when? "
@@ -157,44 +165,44 @@ to be CLOCKED OUT."))))
                            (floor (time-to-seconds (time-subtract now date))
                                   60))
                        (string-to-number (calc-eval input)))))))
-	       (subtractp (memq ch '(?s ?S)))
-	       (barely-started-p (org-time-less-p
-			                  (org-time-subtract last-valid (cdr clock))
-			                  45))
-	       (start-over (and subtractp barely-started-p)))
+           (subtractp (memq ch '(?s ?S)))
+           (barely-started-p (org-time-less-p
+                              (org-time-subtract last-valid (cdr clock))
+                              45))
+           (start-over (and subtractp barely-started-p)))
       (cond
        ((memq ch '(?j ?J))
         (if (eq ch ?J)
-	        (org-clock-resolve-clock clock 'now nil t nil fail-quietly))
+            (org-clock-resolve-clock clock 'now nil t nil fail-quietly))
         (org-clock-jump-to-current-clock clock))
        ((or (null ch)
-	        (not (memq ch '(?k ?K ?g ?G ?s ?S ?C))))
+            (not (memq ch '(?k ?K ?g ?G ?s ?S ?C ?t ?T))))
         (message ""))
        (t
         (org-clock-resolve-clock
          clock (cond
-	            ((or (eq ch ?C)
-		             ;; If the time on the clock was less than a minute before
-		             ;; the user went away, and they've ask to subtract all the
-		             ;; time...
-		             start-over)
-	             nil)
-	            ((or subtractp
-		             (and gotback (= gotback 0)))
-	             last-valid)
-	            ((or (and keep (= keep default))
-		             (and gotback (= gotback default)))
-	             'now)
-	            (keep
-	             (org-time-add last-valid (* 60 keep)))
-	            (gotback
-	             (org-time-since (* 60 gotback)))
-	            (t
-	             (error "Unexpected, please report this as a bug")))
+                ((or (eq ch ?C)
+                     ;; If the time on the clock was less than a minute before
+                     ;; the user went away, and they've ask to subtract all the
+                     ;; time...
+                     start-over)
+                 nil)
+                ((or subtractp
+                     (and gotback (= gotback 0)))
+                 last-valid)
+                ((or (and keep (= keep default))
+                     (and gotback (= gotback default)))
+                 'now)
+                (keep
+                 (org-time-add last-valid (* 60 keep)))
+                (gotback
+                 (org-time-since (* 60 gotback)))
+                (t
+                 (error "Unexpected, please report this as a bug")))
          (and gotback last-valid)
-         (memq ch '(?K ?G ?S))
+         (memq ch '(?K ?G ?S ?T))
          (and start-over
-	          (not (memq ch '(?K ?G ?S ?C))))
+              (not (memq ch '(?K ?G ?S ?C))))
          fail-quietly)))))
 
   (el-patch-defun org-clock-resolve-clock
@@ -202,27 +210,27 @@ to be CLOCKED OUT."))))
     "Resolve CLOCK given the time RESOLVE-TO, and the present.
 CLOCK is a cons cell of the form (MARKER START-TIME)."
     (let ((org-clock-resolving-clocks t)
-	      ;; If the clocked entry contained only a clock and possibly
-	      ;; the associated drawer, and we either cancel it or clock it
-	      ;; out, `org-clock-out-remove-zero-time-clocks' may clear all
-	      ;; contents, and leave point on the /next/ headline.  We store
-	      ;; the current entry location to be able to get back here when
-	      ;; we need to clock in again the previously clocked task.
-	      (heading (org-with-point-at (car clock)
-		             (org-back-to-heading t)
-		             (point-marker))))
+          ;; If the clocked entry contained only a clock and possibly
+          ;; the associated drawer, and we either cancel it or clock it
+          ;; out, `org-clock-out-remove-zero-time-clocks' may clear all
+          ;; contents, and leave point on the /next/ headline.  We store
+          ;; the current entry location to be able to get back here when
+          ;; we need to clock in again the previously clocked task.
+          (heading (org-with-point-at (car clock)
+                     (org-back-to-heading t)
+                     (point-marker))))
       (pcase resolve-to
         (`nil
          (org-clock-clock-cancel clock)
          (when (and restart (not org-clock-clocking-in))
-	       (org-with-point-at heading (org-clock-in))))
+           (org-with-point-at heading (org-clock-in))))
         (`now
          (cond
-	      (restart (error "RESTART is not valid here"))
-	      ((or close org-clock-clocking-in)
-	       (org-clock-clock-out clock fail-quietly))
-	      ((org-is-active-clock clock) nil)
-	      (t (org-clock-clock-in clock t))))
+          (restart (error "RESTART is not valid here"))
+          ((or close org-clock-clocking-in)
+           (org-clock-clock-out clock fail-quietly))
+          ((org-is-active-clock clock) nil)
+          (t (org-clock-clock-in clock t))))
         (el-patch-remove
           ((pred (org-time-less-p nil))
            (error "RESOLVE-TO must refer to a time in the past")))
@@ -230,11 +238,11 @@ CLOCK is a cons cell of the form (MARKER START-TIME)."
          (when restart (error "RESTART is not valid here"))
          (org-clock-clock-out clock fail-quietly (or clock-out-time resolve-to))
          (cond
-	      (org-clock-clocking-in nil)
-	      (close
-	       (setq org-clock-leftover-time (and (null clock-out-time) resolve-to)))
-	      (t
-	       (org-with-point-at heading
-	         (org-clock-in nil (and clock-out-time resolve-to))))))))))
+          (org-clock-clocking-in nil)
+          (close
+           (setq org-clock-leftover-time (and (null clock-out-time) resolve-to)))
+          (t
+           (org-with-point-at heading
+             (org-clock-in nil (and clock-out-time resolve-to))))))))))
 
 (provide 'my-el-patch)
