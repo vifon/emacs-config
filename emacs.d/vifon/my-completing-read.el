a8a3f281a4495a22fef6ebb9fc3dc6f1ebcb7313
Wojciech Siewierski <wojciech@siewierski.eu>
2021-04-03 17:24:45 +0200

    Consult: Tab in isearch calls consult-line

--- emacs.d/vifon/my-completing-read.el
+++ emacs.d/vifon/my-completing-read.el
@@ -71,6 +71,12 @@
          ([remap switch-to-buffer] . consult-buffer)
          ([remap yank-pop] . consult-yank-pop)
          ([remap goto-line] . consult-goto-line))
+  :init (define-key isearch-mode-map
+          (kbd "TAB")
+          (lambda ()
+            (interactive)
+            (isearch-exit)
+            (consult-line isearch-string)))
   :config (progn
             (let ((grep-config (list :preview-key (kbd "TAB")
                                      :keymap (let ((map (make-sparse-keymap)))
