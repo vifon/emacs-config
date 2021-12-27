426c0165dc8d77f89924aaec2c4bcb27802817d6
Wojciech Siewierski <wojciech@siewierski.eu>
2021-12-27 21:15:55 +0100

    Move my skeletons according to the no-littering conventions

--- emacs.d/vifon/my-skeletons.el
+++ emacs.d/vifon/my-skeletons.el
@@ -1,4 +1,4 @@
 ;;; -*- lexical-binding: t; -*-
 
-(dolist (skeleton-file (directory-files "~/.emacs.d/skeletons/" t "\\.elc?$"))
+(dolist (skeleton-file (directory-files "~/.emacs.d/etc/skeletons/" t "\\.elc?$"))
   (load skeleton-file))
