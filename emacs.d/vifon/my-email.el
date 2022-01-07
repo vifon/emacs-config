3302f9b459ebe94ce694086352caa3274d868b7c
Wojciech Siewierski <wojciech@siewierski.eu>
2022-01-07 03:09:36 +0100

    Move the Notmuch config to a separate file, it's already massive

---
+++ emacs.d/vifon/my-email.el
@@ -0,0 +1,114 @@
+;;; -*- lexical-binding: t; -*-
+
+(use-package notmuch
+  :straight t
+  :if (and (file-directory-p "~/Mail/.notmuch")
+           (file-regular-p   "~/.emacs.d/secret/notmuch-fcc"))
+  :bind (("<f7>" . notmuch))
+  :config (progn
+            (setq notmuch-fcc-dirs
+                  (let ((tags "-new -unread -inbox +sent"))
+                    (mapcar
+                     (lambda (rule)
+                       (let ((account (car rule))
+                             (folder  (cdr rule)))
+                         (cons account (format "\"%s\" %s"
+                                               folder tags))))
+                     (with-temp-buffer
+                       ;; Format:
+                       ;;   (("me@example.com"   . "me@example.com/Sent")
+                       ;;    ("alsome@gmail.com" . "alsome@gmail.com/Sent Mail"))
+                       (insert-file-contents "~/.emacs.d/secret/notmuch-fcc")
+                       (goto-char (point-min))
+                       (read (current-buffer))))))
+            (setq message-signature
+                  (lambda ()
+                    (when (eq this-command #'message-insert-signature)
+                      (let* ((signature-override
+                              (concat (file-name-as-directory "~/.signature.d")
+                                      (message-sendmail-envelope-from)))
+                             (signature-file
+                              (if (file-readable-p signature-override)
+                                  signature-override
+                                "~/.signature")))
+                        (when (file-readable-p signature-file)
+                          (with-temp-buffer
+                            (insert-file-contents signature-file)
+                            (buffer-string)))))))
+
+            (setq message-sendmail-envelope-from 'header
+                  send-mail-function 'sendmail-send-it)
+            (setq notmuch-always-prompt-for-sender t)
+
+            (setq message-auto-save-directory nil
+                  message-confirm-send t)
+
+            (setq notmuch-wash-signature-lines-max 3)
+
+            (when (file-executable-p "~/.bin/notmuch-sync")
+              (defun vifon/notmuch-poll-and-refresh-this-buffer ()
+                (interactive)
+                (call-process
+                 "notmuch-sync" nil 0 nil
+                 (buffer-name (current-buffer))))
+              (dolist (map (list notmuch-hello-mode-map
+                                 notmuch-show-mode-map
+                                 notmuch-search-mode-map))
+                (bind-key "G" #'vifon/notmuch-poll-and-refresh-this-buffer
+                          map)))
+
+            (dolist (map (list notmuch-hello-mode-map
+                               notmuch-show-mode-map))
+              (unbind-key "<C-tab>" map))
+
+            (bind-key "A"
+                      (lambda ()
+                        (interactive)
+                        (when (y-or-n-p "Archive all?")
+                          (notmuch-search-tag-all '("-unread" "-inbox"))))
+                      notmuch-search-mode-map)
+            (bind-key "D"
+                      (lambda ()
+                        (interactive)
+                        (when (y-or-n-p "Delete all?")
+                          (notmuch-search-tag-all '("-unread" "-inbox" "+deleted"))))
+                      notmuch-search-mode-map)
+
+
+            (defun notmuch-clear-search-history ()
+              (interactive)
+              (when (y-or-n-p "Clear the notmuch search history? ")
+                (setq notmuch-search-history nil)
+                (notmuch-refresh-this-buffer)))
+            (bind-key "d" #'notmuch-clear-search-history
+                      notmuch-hello-mode-map)
+            (bind-key "C-c C-o"
+                      (lambda (arg)
+                        (interactive "P")
+                        (if arg
+                            (progn
+                              (require 'shr)
+                              (shr-next-link))
+                          (require 'ffap)
+                          (ffap-next-url)))
+                      notmuch-show-mode-map)
+
+            (bind-key "O"
+                      (lambda () (interactive)
+                        (setq notmuch-search-oldest-first
+                              (not notmuch-search-oldest-first))
+                        (message "Notmuch search oldest first: %s"
+                                 notmuch-search-oldest-first))
+                      notmuch-hello-mode-map)
+
+            (defun notmuch-fcc-replace ()
+              (interactive)
+              (message-remove-header "Fcc")
+              (notmuch-fcc-header-setup))
+
+            (setq mm-tmp-directory (file-name-as-directory
+                                    (concat
+                                     "/tmp/mml-" (user-login-name))))
+            (make-directory mm-tmp-directory t)
+
+            (setq mml-secure-openpgp-sign-with-sender t)))
