f3f41b8a909d63a4ef716de732f668b1569edb87
Wojciech Siewierski <wojciech@siewierski.eu>
2022-01-07 01:20:16 +0100

    nix-skeleton: Offer to use the local sources

--- emacs.d/etc/skeletons/nix-skeleton.el
+++ emacs.d/etc/skeletons/nix-skeleton.el
@@ -53,21 +53,23 @@
                                  (directory-file-name
                                   default-directory))) "\";\n"
   "  version = \"" (skeleton-read "Package version: " "1.0") "\";\n"
-  (if (y-or-n-p "Fetch from Github?")
-      (concat "  src = fetchFromGitHub {\n"
-              "    owner = \"" (skeleton-read "Repo owner: " (user-login-name)) "\";\n"
-              "    repo = pname;\n"
-              "    rev = \"v${version}\";\n"
-              "    sha256 = \"\";\n"
-              "  };\n")
-    (concat "  src = fetchurl {\n"
-            "    url = " (setq v1 (skeleton-read "Fetch URL: ")) ";\n"
-            "    sha256 = \"" (when (and (not (string-empty-p v1))
-                                         (y-or-n-p "Fetch & calculate the hash?"))
-                                (shell-command-to-string (concat "nix-prefetch-url "
-                                                                 "\"" v1 "\""
-                                                                 " 2> /dev/null"))) "\";\n"
-            "  };\n"))
+  (if (y-or-n-p "Fetch from the web?")
+      (if (y-or-n-p "Fetch from Github?")
+          (concat "  src = fetchFromGitHub {\n"
+                  "    owner = \"" (skeleton-read "Repo owner: " (user-login-name)) "\";\n"
+                  "    repo = pname;\n"
+                  "    rev = \"v${version}\";\n"
+                  "    sha256 = \"\";\n"
+                  "  };\n")
+        (concat "  src = fetchurl {\n"
+                "    url = " (setq v1 (skeleton-read "Fetch URL: ")) ";\n"
+                "    sha256 = \"" (when (and (not (string-empty-p v1))
+                                             (y-or-n-p "Fetch & calculate the hash?"))
+                                    (shell-command-to-string (concat "nix-prefetch-url "
+                                                                     "\"" v1 "\""
+                                                                     " 2> /dev/null"))) "\";\n"
+                "  };\n"))
+      "  src = lib.cleanSource ./.;\n")
   "\n"
   "  buildInputs = [\n"
   "    pkg-config" _ "\n"
