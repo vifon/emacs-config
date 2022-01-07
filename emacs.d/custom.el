046ddc0916b3e8cad6efb1b027a19b474ab84b50
Wojciech Siewierski <wojciech@siewierski.eu>
2022-01-07 01:08:24 +0100

    Remove all the face customization
    Let's see which ones I actually still use and/or need.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -21,36 +21,6 @@
  '(calendar-mark-holidays-flag nil)
  '(calendar-week-start-day 1))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(diff-added ((((background dark)) (:foreground "green"))))
- '(diff-changed ((((background dark)) (:foreground "yellow"))))
- '(diff-context ((((background dark)) (:foreground "White")) (t (:foreground "Black"))))
- '(diff-file-header ((((background dark)) (:foreground "Cyan" :background "Black")) (t (:foreground "Red" :background "White"))))
- '(diff-header ((((background dark)) (:foreground "Cyan")) (t (:foreground "Red"))))
- '(diff-hl-change ((t (:background "yellow3" :foreground "yellow2"))))
- '(diff-hl-delete ((t (:background "firebrick" :foreground "red"))))
- '(diff-hl-insert ((t (:background "lime green" :foreground "green"))))
- '(diff-hunk-header ((((background dark)) (:foreground "Black" :background "#05057F7F8D8D")) (t (:foreground "White" :background "Salmon"))))
- '(diff-index ((((background dark)) (:foreground "Magenta")) (t (:foreground "Green"))))
- '(diff-indicator-added ((t (:inherit diff-added))))
- '(diff-indicator-changed ((t (:inherit diff-changed))))
- '(diff-indicator-removed ((t (:inherit diff-removed))))
- '(diff-nonexistent ((((background dark)) (:foreground "#FFFFFFFF7474")) (t (:foreground "DarkBlue"))))
- '(diff-refine-added ((((background dark)) (:inherit diff-added :background "green4")) (t (:inherit diff-added :background "#88ee88"))))
- '(diff-refine-removed ((((background dark)) (:inherit diff-removed :background "red4")) (t (:inherit diff-removed :background "#ff9999"))))
- '(diff-removed ((((background dark)) (:foreground "red"))))
- '(notmuch-search-unread-face ((t (:weight extra-bold))))
- '(term-color-blue ((t (:background "#4276b8" :foreground "#4276b8"))))
- '(term-color-cyan ((t (:background "#0b939b" :foreground "#0b939b"))))
- '(term-color-green ((t (:background "#00cc00" :foreground "#00cc00"))))
- '(term-color-magenta ((t (:background "#95709b" :foreground "#95709b"))))
- '(term-color-red ((t (:background "#dc0000" :foreground "#dc0000"))))
- '(term-color-yellow ((t (:background "#c4a000" :foreground "#c4a000")))))
-
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
