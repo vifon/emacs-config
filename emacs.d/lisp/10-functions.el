caf36823448ac6563be0651ab8265c7949dbe717
Wojciech Siewierski <wojciech@siewierski.eu>
2022-01-12 17:07:51 +0100

    Remove the no longer used functions

--- emacs.d/lisp/10-functions.el
+++ emacs.d/lisp/10-functions.el
@@ -3,28 +3,6 @@
 (require 'cl-lib)
 
 
-;;; used by yasnippet
-(defun get-c-arg-names (arg-list)
-  (mapconcat
-   (lambda (arg)
-     (replace-regexp-in-string "^.*?\\(\\w*\\)[[:blank:]]*$" "\\1" arg))
-   (split-string text ",")
-   ", "))
-(defun get-c-arg-types (arg-list)
-  (mapconcat
-   (lambda (arg)
-     (replace-regexp-in-string "^[[:blank:]]*\\(.*?\\)[[:blank:]]*\\w*[[:blank:]]*$" "\\1" arg))
-   (split-string text ",")
-   ", "))
-
-(defun c-c++-header ()
-  "Sets either c-mode or c++-mode, whichever is appropriate."
-  (interactive)
-  (let ((c-file (concat (substring (buffer-file-name) 0 -1) "c")))
-    (if (file-exists-p c-file)
-        (c-mode)
-        (c++-mode))))
-
 (defun vifon/scratch-dir-path (name)
   (concat "~/scratch.d/scratch-"
           (format-time-string "%Y-%m-%d_%s")
@@ -50,94 +28,6 @@ Prefix argument GIT initializes it as a Git repository."
       (let ((default-directory scratch))
         (vc-git-create-repo)))
     (dired scratch)))
-
-(defun call-with-default-completing-read (oldfun &rest args)
-  "Call `oldfun' with a `completing-read-function' set to `completing-read-default'.
-
-`ivy-completing-read' and possibly other custom
-`completing-read-function' values do interfere with some
-commands."
-  (let ((completing-read-function #'completing-read-default))
-    (apply oldfun args)))
-
-(defun cc-extract ()
-  "Save the full function body to the kill-ring and replace it with a declaration."
-  (interactive)
-  (save-excursion
-    (save-restriction
-      (narrow-to-defun)
-      (let* ((function-body (buffer-substring-no-properties
-                             (point-min)
-                             (point-max)))
-             (function-indent ;get the absolute indent of the function body
-              (replace-regexp-in-string "^\\([[:blank:]]*\\).*" "\\1"
-                                        (car (split-string function-body "\n"))))
-             (deindented-function-body
-              (replace-regexp-in-string (concat "^"
-                                                function-indent)
-                                        ""
-                                        function-body)))
-        (kill-new deindented-function-body)
-        (goto-char (1- (point-max)))    ;1- because we do not want to
-                                        ;delete the final newline
-        (let ((body-end (point)))
-          (backward-sexp)               ;go to the beginning of the body
-          (let ((body-begin (point)))
-            (delete-region body-begin
-                           body-end))
-          (delete-horizontal-space)
-          (let ((tmp (point)))
-            (skip-chars-backward " \n\t")
-            (delete-region (point)      ;remove the leftover whitechars
-                           tmp))
-          (insert ";"))))))
-
-(defun cc-headerize ()
-  "Save the function header to the kill-ring."
-  (interactive)
-  (save-excursion
-    (save-restriction
-      (narrow-to-defun)
-      (goto-char (point-min))
-      (skip-chars-forward " \n\t")
-      (let ((function-begin (point)))
-        (goto-char (point-max))
-        (backward-sexp)
-        (skip-chars-backward " \n\t")
-        (let ((function-header
-               (buffer-substring-no-properties function-begin
-                                               (point))))
-          (kill-new function-header)
-          (message "Header copied: %s" function-header))))))
-
-(defun flags-if-supported (command &rest flags)
-  "Return each flag in FLAGS unchanged if COMMAND supports it
-according to the manpage, otherwise return nil in its place.
-
-The flag is considered supported if it appears anywhere in the
-manpage."
-  (with-temp-buffer
-    (process-file "man" nil t nil command)
-    (mapcar (lambda (flag)
-              (goto-char (point-min))
-              (when (re-search-forward (concat "\\_<"
-                                               (regexp-quote flag)
-                                               "\\_>")
-                                       nil t)
-                flag))
-            flags)))
-
-(defun flags-nonportable (portable-flags command &rest nonportable-flags)
-  "Combine PORTABLE-FLAGS and the supported NONPORTABLE-FLAGS
-into a single string with `combine-and-quote-strings'.
-
-See also: `flags-if-supported'."
-  (combine-and-quote-strings
-   (cl-delete-if
-    nil (cons portable-flags
-              (apply #'flags-if-supported
-                     command
-                     nonportable-flags)))))
 
 (defun dired-import-ranger-tags ()
   (interactive)
