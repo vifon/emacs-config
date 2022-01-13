2bb1c4367e133bfda5040eaf2704d89f2104adff
Wojciech Siewierski <wojciech@siewierski.eu>
2022-01-13 23:25:57 +0100

    Use the minibuffer-based UI for M-tab (completion-at-point)

--- emacs.d/lisp/20-completing-read.el
+++ emacs.d/lisp/20-completing-read.el
@@ -119,3 +119,12 @@
 
 (setq enable-recursive-minibuffers t)
 (minibuffer-depth-indicate-mode 1)
+
+;;; Use the completing-read UI for the M-tab completion.
+(setq completion-in-region-function
+      (lambda (&rest args)
+        (apply (if vertico-mode
+                   #'consult-completion-in-region
+                 #'completion--in-region)
+               args)))
+(setq completion-cycle-threshold 3)
