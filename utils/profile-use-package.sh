1fe39234700e4ce801e19fee30db2b90ec851938
Wojciech Siewierski <wojciech@siewierski.eu>
2022-01-14 01:23:04 +0100

    Fix the profiling script

--- utils/profile-use-package.sh
+++ utils/profile-use-package.sh
@@ -3,6 +3,6 @@
 set -o errexit -o nounset -o pipefail
 
 emacs -Q \
-      --eval '(with-eval-after-load "use-package" (setq use-package-compute-statistics t))' \
+      --eval '(setq use-package-compute-statistics t)' \
       -l ~/.emacs.d/init.el \
       -f use-package-report
