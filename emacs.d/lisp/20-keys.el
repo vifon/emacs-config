0b5ab8d100c94635238e1147e8a72f7ccbdcfe35
Wojciech Siewierski <wojciech@siewierski.eu>
2022-01-15 14:17:10 +0100

    Move the still used "pastes-from-web" to my custom keybinds file

--- emacs.d/lisp/20-keys.el
+++ emacs.d/lisp/20-keys.el
@@ -131,6 +131,32 @@ ends with a newline."
 
 (bind-key "C-x C-M-t" #'transpose-regions)
 
+;;; http://endlessparentheses.com/emacs-narrow-or-widen-dwim.html
+(defun narrow-or-widen-dwim (p)
+  "If the buffer is narrowed, it widens. Otherwise, it narrows intelligently.
+Intelligently means: region, subtree, or defun, whichever applies
+first.
+
+With prefix P, don't widen, just narrow even if buffer is already
+narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning) (region-end)))
+        ((derived-mode-p 'org-mode) (org-narrow-to-subtree))
+        (t (narrow-to-defun))))
+(bind-key "C-c l" #'narrow-or-widen-dwim)
+
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+(bind-key "M-Q" #'unfill-paragraph)
+
+
 (bind-key "<f9>" #'menu-bar-open)
 
 (windmove-default-keybindings)
