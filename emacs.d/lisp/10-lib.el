40600072060457a22680ee7b9039c30e2cd15007
Wojciech Siewierski <wojciech@siewierski.eu>
2022-01-16 01:35:08 +0100

    Separate the personal commands from personal functions library

---
+++ emacs.d/lisp/10-lib.el
@@ -0,0 +1,12 @@
+;;; -*- lexical-binding: t; -*-
+
+(require 'cl-lib)
+
+(defun vifon/add-to-list-after (list-var old new &optional compare-fn)
+  (let ((cmp (or compare-fn #'equal)))
+    (cl-do ((x (symbol-value list-var) (cdr x)))
+        ((or (null x)
+             (funcall cmp (cadr x) new)))
+      (when (funcall cmp (car x) old)
+        (setf (cdr x) (cons new (cdr x))))))
+  (symbol-value list-var))
