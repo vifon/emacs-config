bad079cf7864c982696f591d1bfb8dfb37c1775d
Wojciech Siewierski <wojciech@siewierski.eu>
2022-02-05 22:40:02 +0100

    Fix the C & C++ skeletons

--- emacs.d/etc/skeletons/c++-skeleton.el
+++ emacs.d/etc/skeletons/c++-skeleton.el
@@ -1,18 +1,18 @@
 (define-skeleton c++-skeleton
   "" ""
-  (setq v1 (concat (file-name-base (buffer-file-name)) ".hpp"))
+  '(setq v1 (concat (file-name-base (buffer-file-name)) ".hpp"))
   (let ((header v1))
     (if (file-exists-p header)
-        (concat "\n#include \"" header "\"\n\n"
+        (concat "#include \"" header "\"\n\n"
                 (when (progn
                         (setq v2 (skeleton-read "Namespace: "))
                         (not (string= "" v2)))
                   (concat "namespace " v2 " {\n\n")))
-        (concat "\n#include <iostream>\n"
-                "#include <string>\n\n"
+      (concat "#include <iostream>\n"
+              "#include <string>\n\n"
 
-                "int main(int argc, char *argv[])\n"
-                "{\n    ")))
+              "int main(int argc, char *argv[])\n"
+              "{\n    ")))
     _
     (if (not (file-exists-p v1))
         (concat "\n    return 0;\n"
@@ -23,7 +23,7 @@
 
 (define-skeleton c-skeleton
   "" ""
-  (setq v1 (concat (file-name-base (buffer-file-name)) ".h"))
+  '(setq v1 (concat (file-name-base (buffer-file-name)) ".h"))
   (let ((header v1))
     (if (file-exists-p header)
         (concat "#include \"" header "\"\n\n")
@@ -39,7 +39,7 @@
 
 (define-skeleton c++-header-skeleton
   "" ""
-  (and (setq v1 (skeleton-read "Namespace: ")) nil)
+  '(setq v1 (skeleton-read "Namespace: "))
   "#pragma once\n\n"
   (when (not (string= "" v1))
     (concat "namespace " v1 " {\n\n"))
