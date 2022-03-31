a0dbc98a13bdd0191ba96c1f514c0e78fe18e664
Wojciech Siewierski <wojciech@siewierski.eu>
2022-03-31 16:17:51 +0200

    Add live-lossage-mode

--- emacs.d/lisp/15-modes.el
+++ emacs.d/lisp/15-modes.el
@@ -7,3 +7,11 @@
   (if gofmt-global-mode
       (add-hook 'before-save-hook #'gofmt-before-save)
     (remove-hook 'before-save-hook #'gofmt-before-save)))
+
+(define-minor-mode live-lossage-mode
+  "Refresh and redisplay the lossage buffer after each command."
+  :global t
+  :lighter " lossage"
+  (if live-lossage-mode
+      (add-hook 'post-command-hook #'view-lossage)
+    (remove-hook 'post-command-hook #'view-lossage)))
