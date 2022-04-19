014d0b992b9cee14ceacccc9c7e1249f9c46b78a
Wojciech Siewierski <wojciech@siewierski.eu>
2022-04-19 10:34:18 +0200

    Document and improve the custom vifon/add-to-list-after function

--- emacs.d/lisp/10-lib.el
+++ emacs.d/lisp/10-lib.el
@@ -1,10 +1,16 @@
 ;;; -*- lexical-binding: t; -*-
 
 (defun vifon/add-to-list-after (list-var old new &optional compare-fn)
+  "Add NEW after OLD in the ordered list LIST-VAR.
+
+OLD is compared with COMPARE-FN which defaults to `equal'.
+
+NEW is not added if it already exists after OLD, also according
+to COMPARE-FN, making this function idempotent."
   (let ((cmp (or compare-fn #'equal)))
     (cl-do ((x (symbol-value list-var) (cdr x)))
-        ((or (null x)
-             (funcall cmp (cadr x) new)))
-      (when (funcall cmp (car x) old)
+        ((null x))
+      (when (and (funcall cmp (car x) old)
+                 (not (funcall cmp (cadr x) new)))
         (setf (cdr x) (cons new (cdr x))))))
   (symbol-value list-var))
