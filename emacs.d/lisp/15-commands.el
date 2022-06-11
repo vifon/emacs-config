7656474aeae3cba04eea81b4325f130fbd9f49e7
Wojciech Siewierski <wojciech@siewierski.eu>
2022-06-12 01:15:15 +0200

    Bring back my old ansi-colorize-buffer command
    It used to be present in my config, later removed deemed
    unnecessary.  Wrong.
    
    It was also improved, BTW.

--- emacs.d/lisp/15-commands.el
+++ emacs.d/lisp/15-commands.el
@@ -43,3 +43,9 @@ Prefix argument GIT initializes it as a Git repository."
 (defun vifon/shrink-all-windows-if-larger-than-buffer ()
   (interactive)
   (mapcar #'shrink-window-if-larger-than-buffer (window-list)))
+
+(defun ansi-colorize-buffer ()
+  (interactive)
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max)))
+  (set-buffer-modified-p nil))
