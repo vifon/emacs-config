3f6f0dbd2135d3013059b626587afd2b85228a48
Wojciech Siewierski <wojciech@siewierski.eu>
2022-06-21 02:05:28 +0200

    Add a Golang testing skeleton

--- emacs.d/etc/skeletons/go-skeleton.el
+++ emacs.d/etc/skeletons/go-skeleton.el
@@ -17,7 +17,22 @@
   "}\n"
   )
 
+(define-skeleton go-test-skeleton
+  "" ""
+  "package " (file-name-nondirectory
+              (directory-file-name
+               (file-name-directory
+                (buffer-file-name))))
+  "\n\n"
+  "import \"testing\"\n"
+  "\n"
+  "func Test" _ "(t *testing.T) {\n"
+  "\tt.Errorf(\"Not implemented\")\n"
+  "}\n"
+  )
+
 (define-auto-insert "\\.go\\'" #'go-skeleton)
 (define-auto-insert "/main\\.go\\'" #'go-main-skeleton)
+(define-auto-insert "_test\\.go\\'" #'go-test-skeleton)
 
 (provide 'go-skeleton)
